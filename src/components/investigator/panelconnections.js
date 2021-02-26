7e4524badd5afdf77201171c734b09b5c8a6289b
storrellas <storrellas@gmail.com>
2021-02-26 16:19:29 +0100

    ENH: Removed unused code

--- src/components/investigator/panelconnections.js
+++ src/components/investigator/panelconnections.js
@@ -29,17 +29,6 @@ class PanelConnections extends React.Component {
 
 
   render() {
-
-
-    const elements = [
-      { data: { id: 'one', label: 'Node 1' }, position: { x: 100, y: 100 } },
-      { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-      { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-    ];
-
-    const img_default_user_gray = '/img/user_gray.png'
-    const img_default_user = '/img/user.png'
-
     return (
       <div>
         <CytoscapeComponent 
