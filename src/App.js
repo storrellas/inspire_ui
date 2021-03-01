d3abe9670d48794da27425e556213e6fc1688ba0
storrellas <storrellas@gmail.com>
2021-03-01 13:58:20 +0100

    ENH: Responsive on login and base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import { Container } from 'react-bootstrap';
-import { Navbar, Nav, NavDropdown, Dropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Col, Nav, NavDropdown, Dropdown, Row } from 'react-bootstrap';
 import { Switch, Route } from "react-router-dom";
 
 import { withRouter } from 'react-router-dom'
@@ -35,132 +37,77 @@ class App extends React.Component {
 
     const isLogin = this.props.location.pathname == '/'
     return (
-      <Container fluid className='inspire-main'>
+      <div className='inspire-main'>
+        <Navbar variant="dark" className="inspire-navbar navbar" expand="sm">
 
-      <Navbar variant="dark" className="inspire-navbar navbar" expand="lg" style={{padding: '0 10% 0 10%'}}>
-        <Navbar.Brand href="#home" className="h-100"><img src={inspire_logo} alt="logo" style={{ height: '100%' }}></img></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="h-100 justify-content-end">
-          <Nav className="h-100 align-items-center">
-            <Nav.Link href="#home" className="inspire-dropdown" >
-              <FontAwesomeIcon style={{ color: 'white' }} icon={faDirections} />
-            </Nav.Link>
-            <Nav.Link href="#link">
-            <FontAwesomeIcon style={{ color: 'white' }} icon={faStar} />
-            </Nav.Link>
-            <NavDropdown title="Project" bsPrefix="inspire-reponsive-dropdown">
-              <NavDropdown.Item href="/project/123">Project1</NavDropdown.Item>
-              <NavDropdown.Item href="/project/123">Project2</NavDropdown.Item>
-              <NavDropdown.Item href="/project/123">Project3</NavDropdown.Item>
-              <NavDropdown.Item href="/project/123">Project4</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Custom" bsPrefix="inspire-reponsive-dropdown">
-              <NavDropdown.Item href="#">Authors</NavDropdown.Item>
-              <NavDropdown.Item href="#">Market Access</NavDropdown.Item>
-              <NavDropdown.Item href="#">Company Cooperation / Conflict of Interest</NavDropdown.Item>
-              <NavDropdown.Item href="#">Other</NavDropdown.Item>
-            </NavDropdown>
-            <Dropdown>
-            <Dropdown.Toggle className="inspire-dropdown-toggle" variant="success" id="dropdown-basic"
-              style={{ backgroundColor: 'transparent', borderColor: 'white' }}>
-              <FontAwesomeIcon style={{ color: 'white' }} icon={faUser} />
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu align="right">
-              <Dropdown.Item href="#/action-1">Sergi Torrellas</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          </Nav>
-
-        </Navbar.Collapse>
-      </Navbar>
-
-        {/* <Navbar variant="dark" className="inspire-navbar navbar" style={{ padding: '0 10% 0 10%' }}>
-          <Navbar.Brand href="#home" style={{ height: '100%' }}>
-            <img src={inspire_logo} alt="logo" style={{ height: '100%' }}></img>
+          <Navbar.Brand href="#home" className="h-100">
+            <img src={inspire_logo} alt="logo" style={{ height: '50px' }}></img>
           </Navbar.Brand>
-          <Nav className="h-100 ml-auto w-50 d-flex justify-content-end">
-
-
-            {isLogin ? '' :
-              <div className="inspire-dropdown d-flex align-items-center" style={{ padding: '0 1em 0 1em' }}>
-                <Dropdown >
-                  <Dropdown.Toggle className="inspire-dropdown-toggle" variant="success" id="dropdown-basic" style={{ backgroundColor: 'transparent', borderColor: 'white' }}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="h-100 justify-content-end">
+            <Nav className="h-100 inspire-nav" style={{ backgroundColor: '#343547' }}>
+              {isLogin ? '' :
+                <Nav.Link href="#home">
+                  <div className="inspire-navbar-link" style={{ display: 'inline', padding: '0.5em' }}>
                     <FontAwesomeIcon style={{ color: 'white' }} icon={faDirections} />
-                  </Dropdown.Toggle>
-                </Dropdown>
-              </div>
-            }
-            {isLogin ? '' :
-              <div className="inspire-dropdown d-flex align-items-center" style={{ padding: '0 1em 0 1em' }}>
-                <Dropdown >
-                  <Dropdown.Toggle className="inspire-dropdown-toggle" variant="success" id="dropdown-basic" style={{ backgroundColor: 'transparent', borderColor: 'white' }}>
+                  </div>
+                </Nav.Link>
+              }
+
+              {isLogin ? '' :
+                <Nav.Link href="#link">
+                  <div className="inspire-navbar-link" style={{ display: 'inline', padding: '0.5em' }}>
                     <FontAwesomeIcon style={{ color: 'white' }} icon={faStar} />
-                  </Dropdown.Toggle>
+                  </div>
+                </Nav.Link>
+              }
+
+              {isLogin ? '' :
+                <NavDropdown title="Project" bsPrefix="inspire-reponsive-dropdown">
+                  <NavDropdown.Item href="/project/123">Project1</NavDropdown.Item>
+                  <NavDropdown.Item href="/project/123">Project2</NavDropdown.Item>
+                  <NavDropdown.Item href="/project/123">Project3</NavDropdown.Item>
+                  <NavDropdown.Item href="/project/123">Project4</NavDropdown.Item>
+                </NavDropdown>
+              }
+              {isLogin ? '' :
+                <NavDropdown title="Custom" bsPrefix="inspire-reponsive-dropdown">
+                  <NavDropdown.Item href="#">Authors</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Market Access</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Company Cooperation / Conflict of Interest</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Other</NavDropdown.Item>
+                </NavDropdown>
+              }
+
+              {isLogin ? '' :
+                <Dropdown>
+                  <div style={{ margin: '0.5em' }}>
+                    <Dropdown.Toggle className="inspire-dropdown-toggle" bsPrefix="test" variant="success" id="dropdown-basic">
+                      <FontAwesomeIcon icon={faUser} />
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu align="right">
+                      <Dropdown.Item href="#/action-1">Sergi Torrellas</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </div>
                 </Dropdown>
-              </div>
-            }
-            {isLogin ? '' :
-              <div className="inspire-dropdown d-flex align-items-center">
-                <Dropdown >
-                  <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ backgroundColor: 'transparent', border: 0, boxShadow: 'none' }}>
-                    Project
-                </Dropdown.Toggle>
+              }
+            </Nav>
 
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="/project/123">Project1</Dropdown.Item>
-                    <Dropdown.Item href="/project/123">Project2</Dropdown.Item>
-                    <Dropdown.Item href="/project/123">Project3</Dropdown.Item>
-                    <Dropdown.Item href="/project/123">Project4</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            }
-            {isLogin ? '' :
-              <div className="inspire-dropdown d-flex align-items-center mr-3">
-                <Dropdown >
-                  <Dropdown.Toggle variant="success" id="dropdown-basic" style={{ backgroundColor: 'transparent', border: 0, boxShadow: 'none' }}>
-                    Custom
-                </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Authors</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Market Access</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Company Cooperation / Conflict of Interest</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Other</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            }
-
-            {isLogin ? '' :
-              <div className="d-flex align-items-center">
-                <Dropdown >
-                  <Dropdown.Toggle className="inspire-dropdown-toggle" variant="success" id="dropdown-basic"
-                    style={{ backgroundColor: 'transparent', borderColor: 'white' }}>
-                    <FontAwesomeIcon style={{ color: 'white' }} icon={faUser} />
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu align="right">
-                    <Dropdown.Item href="#/action-1">Sergi Torrellas</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            }
-
-          </Nav>
-        </Navbar> */}
-        <div className="inspire-content">
-          <Switch>
-            <Route path={`${this.props.match.path}/`} exact component={Login} />
-            <Route path={`${this.props.match.path}project`} exact component={ProjectSelector} />
-            <Route path={`${this.props.match.path}project/:id`} exact component={InvestigatorList} />
-            <Route path={`${this.props.match.path}project/:id/investigator/:subid`} exact component={Investigator} />
-          </Switch>
-        </div>
-      </Container>);
+          </Navbar.Collapse>
+        </Navbar>
+        <Container>
+          <Row className="inspire-content">
+            <Col sm={12}>
+              <Route path={`${this.props.match.path}/`} exact component={Login} />
+              <Route path={`${this.props.match.path}project`} exact component={ProjectSelector} />
+              <Route path={`${this.props.match.path}project/:id`} exact component={InvestigatorList} />
+              <Route path={`${this.props.match.path}project/:id/investigator/:subid`} exact component={Investigator} />
+              </Col>
+          </Row>
+        </Container>
+      </div>);
   }
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row } from 'react-bootstrap';
 
 import { withRouter } from 'react-router-dom'
 
@@ -20,39 +21,44 @@ class Login extends React.Component {
 
   render() {
     return (
+      <Row>
+        <Col sm={12} style={{
+          backgroundColor: 'white', border: '1px solid',
+          borderColor: 'transparent #dee2e6 #dee2e6 #dee2e6', borderRadius: '0 .25rem 0 .25rem',
+          minHeight: '50vh', padding: '2em'
+        }}>
 
-      <div style={{
-        backgroundColor: 'white', border: '1px solid',
-        borderColor: 'transparent #dee2e6 #dee2e6 #dee2e6', borderRadius: '0 .25rem 0 .25rem',
-        minHeight: '50vh', padding: '2em'
-      }}>
-        <div className="w-100">
-          <div style={{ borderBottom: '1px solid #ccc', color: 'grey', background: 'white'}}>LOGIN</div>
-
-          <div style={{ margin: '0 auto', width: '400px', padding: '3em 0 3em 0'}}>
-            
-
-            <div>
-              <input type="text" className="form-control text-center" 
-                  placeholder="Username" style={{ height: '55px'}}></input>
-            </div>
-            <div style={{ paddingTop: '1em'}}>
-              <input type="password" className="form-control text-center" 
-                  placeholder="Password" style={{ height: '55px'}}></input>
-            </div>
-            <div style={{ paddingTop: '1em'}}>
-              <button type="submit" className="btn btn-block" 
-                  style={{height:'55px', fontSize:'16px', fontWeight:'bold', backgroundColor: '#4285f4', color: 'white'}}
-                  onClick={(e) => this.props.history.push('/project')}>Login</button>
+          <div className="w-100">
+            <div style={{ borderBottom: '1px solid #ccc', color: 'grey'}}>
+              LOGIN
             </div>
 
-            <div style={{ paddingTop: '1em'}}>
-              <p className="text-justify" style={{color:'#C9302C', fontSize: '12px'}}>
-                If you experience problems, contact <a href="mailto:support@explicatos.com">support@explicatos.com</a></p>
+            <div style={{ margin: '0 auto', padding: '3em 10% 3em 10%' }}>
+
+              <div>
+                <input type="text" className="form-control text-center"
+                  placeholder="Username" style={{ height: '55px' }}></input>
               </div>
+              <div style={{ paddingTop: '1em' }}>
+                <input type="password" className="form-control text-center"
+                  placeholder="Password" style={{ height: '55px' }}></input>
+              </div>
+              <div style={{ paddingTop: '1em' }}>
+                <button type="submit" className="btn btn-block"
+                  style={{ height: '55px', fontSize: '16px', fontWeight: 'bold', backgroundColor: '#4285f4', color: 'white' }}
+                  onClick={(e) => this.props.history.push('/project')}>Login</button>
+              </div>
+
+              <div style={{ paddingTop: '1em' }}>
+                <p className="text-justify" style={{ color: '#C9302C', fontSize: '12px' }}>
+                  If you experience problems, contact <a href="mailto:support@explicatos.com">support@explicatos.com</a></p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>);
+
+        </Col>
+        
+      </Row>);
   }
 }
 
