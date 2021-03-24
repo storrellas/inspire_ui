254881f193a14499f9ac71fc9111e97a6c8c6cd6
storrellas <storrellas@gmail.com>
2021-03-24 13:36:06 +0100

    ENH: Added Feedback

--- src/components/investigator/panelfeedback.js
+++ src/components/investigator/panelfeedback.js
@@ -2,6 +2,9 @@ import React from 'react';
 // React Router
 import { withRouter } from 'react-router-dom'
 
+// Bootstrap
+import { Form } from 'react-bootstrap';
+
 class PanelFeedback extends React.Component {
 
   constructor(props) {
@@ -15,10 +18,42 @@ class PanelFeedback extends React.Component {
 
     return (
       <div>
-        <div style={{ padding: '1em 1em 1em 1em'}}>
-          <textarea className="w-100" rows="8" ></textarea>
+        <div style={{ padding: '1em 20% 1em 20%'}}>
+
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Ticket Type</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Ticket Priority</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Label className="mt-2">
+            Feedback Comment
+          </Form.Label>
+          <textarea className="w-100" rows="8" style={{ borderColor: '#ced4da', resize: 'none'}}>            
+          </textarea>
+
           <div className="w-100" style={{ padding: '0.2em'}}>
-            <button className="btn btn-primary" style={{ backgroundColor: '#4780c4', fontSize: '14px', width: '100%'  }}>Send</button>
+            <button className="mt-3 w-100 inspire-button"
+                style={{ paddingLeft: '1em', paddingRight: '1em' }}
+                onClick={ (e) => this.showTableUniversities()}>
+                Send Feedback
+            </button>
           </div>
         </div>
       </div>);
